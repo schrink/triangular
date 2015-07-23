fe3f479b59d5b88b9ce5b9d7843ea48187ddb025
v.lelicanin <v.lelicanin@sae.edu>
2015-07-23 23:25:30 +0200

    initial

---
+++ triangular.js
@@ -0,0 +1,27 @@
+(function ( $ ) {
+    $.fn.triangles = function( options ) {
+        var settings = $.extend({
+            // These are the defaults.
+            noOfTriangles: 200,
+            offsetTrigger: "30%"
+        }, options );
+ 
+        for (var i = 0; i < settings.noOfTriangles; i++) {
+    $('<div>').appendTo(this);
+  }
+      var $that = $(this); 
+      $that.addClass('triangular');
+  
+        $that.children('div').waypoint({
+  handler: function(direction) {
+    var index = $that.children('div').index(this.element);
+    $('.active-odd, .active-even').removeClass('active-odd active-even');
+    var classToAdd = (index % 2 == 1) ? "active-even" : "active-odd";
+    $(this.element).addClass(classToAdd);
+  }, 
+   offset: settings.offsetTrigger
+  });
+ 
+    };
+ 
+}( jQuery ));
